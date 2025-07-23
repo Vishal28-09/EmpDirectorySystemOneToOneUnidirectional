a09f57484461d2f34202e35a1abce843cf406ef4
Vishal Vahatule <vwahatule7@gmail.com>
2025-07-24 01:34:21 +0530

    Add Employee Details entity with OneToOne mapping and getters/setters.

---
+++ src/main/java/com/mapping/Entity/EmpDetails.java
@@ -0,0 +1,47 @@
+package com.mapping.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee_details")
+public class EmpDetails {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String address;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	private String number;
+	
+
+}
