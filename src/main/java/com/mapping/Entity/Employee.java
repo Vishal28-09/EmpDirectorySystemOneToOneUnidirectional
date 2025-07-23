142160bbc59f20543064e39be149db1368c42edb
Vishal Vahatule <vwahatule7@gmail.com>
2025-07-24 01:36:18 +0530

    Add Employee entity with mapping to Employee Details and getters/setters.

---
+++ src/main/java/com/mapping/Entity/Employee.java
@@ -0,0 +1,75 @@
+package com.mapping.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String empName;
+	
+	private String designation;
+	
+	private double salary;
+	
+	private String department;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", empName=" + empName + ", designation=" + designation + ", salary=" + salary
+				+ ", department=" + department + "]";
+	}
+	
+	
+	
+
+}
